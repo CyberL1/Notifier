690a7d0e4e83899e231c525141e54bc25e44fa17
CyberL1 <mateusz.hf18@gmail.com>
2024-11-21 12:46:30 +0100

    fix: channel args

--- src/utils/sendToChannel.ts
+++ src/utils/sendToChannel.ts
@@ -4,12 +4,7 @@ import { debugLog } from "./debugLog.ts";
 export const sendToChannel = async (channel: Channel, args: object) => {
   try {
     if (channel.enabled) {
-      const dataStrigified = JSON.stringify(channel.data).replace(
-        /{{(.+)}}/g,
-        (_$1, $2) => args[$2],
-      );
-
-      channel.data = JSON.parse(dataStrigified);
+      channel.data = args;
 
       (await import(`#channels/${channel.type}.ts`)).run(channel);
     }
