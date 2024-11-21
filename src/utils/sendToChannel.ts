7233df32d76101c7813995af847cde9215a832f3
CyberL1 <mateusz.hf18@gmail.com>
2024-11-21 19:14:11 +0100

    feat: use mustache for templating

--- src/utils/sendToChannel.ts
+++ src/utils/sendToChannel.ts
@@ -1,27 +1,13 @@
 import type { Channel } from "#src/types.ts";
+import mustache from "mustache";
 import { debugLog } from "./debugLog.ts";
 
 export const sendToChannel = async (channel: Channel, args: object) => {
   try {
     if (channel.enabled) {
-      const data = { ...channel.data };
+      let data = { ...channel.data };
 
-      const replacePlaceholders = (data: any, args: object) => {
-        for (const key of Object.keys(data)) {
-          if (typeof data[key] === "string") {
-            data[key] = data[key].replace(
-              /{{(.+?)}}/g,
-              ($1: string, $2: string) => args[$2] ?? $1,
-            );
-          } else if (typeof data[key] === "object" && data[key]) {
-            data[key] = replacePlaceholders(data[key], args);
-          }
-        }
-
-        return data;
-      };
-
-      replacePlaceholders(data, args);
+      data = JSON.parse(mustache.render(JSON.stringify(data), args));
 
       (await import(`#channels/${channel.type}.ts`)).run({ ...channel, data });
     }
